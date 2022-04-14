38856e1bcc1b0ca63b41201c567557aebb19d770
Emerson R S Anjos <emerson_qi_10.0@hotmail.com>
2022-04-14 08:17:46 -0300

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+game 'gta5'
+
+files {
+	'background.png',
+	'logo.png',
+	'discord.png',
+	'DantZera1.png',
+	'DantZera1c.png',
+	'audio.ogg',
+	'background.mp4',
+	
+	'smoke.png',
+	'smoke-2.png',
+	'grid.png',	
+	'three.js',
+	'settings.js',
+	'app.js',
+	'player.js',
+	'smoke.js',
+	'logo.js',
+	'style.css',
+	'index.html'
+}
+
+loadscreen 'index.html'
---
+++ settings.js
@@ -0,0 +1,20 @@
+//set type of version to use 1 for static logo and 2 for animate
+var version = 1 // setting version from 1 and 2		
+
+//settings for audio player
+var AudioFile = 'audio.ogg'; //music
+var AudioVolume = 0.3; //volume at start from 0 to 1
+var ColorWawe = '#d37902'; // color player wawe arround logo
+
+//background
+var UseVideo = false; //set true for video and false for IMG on background
+var BackgroundFile = 'https://i.pinimg.com/originals/40/fb/cb/40fbcb26bbfd07154fca64a8e4c51650.jpg'; //background IMG
+
+//other settings
+var img_logo1 = 'DantZera1.png'; //LOGO
+var img_logo2 = 'DantZera1.png'; //second IMG on center for discord -- ONLY on version 2
+var Discord = 'discord.gg/u3j9NZ7'; //discord text visibile -- ONLY on version 1
+var ColorTXT = '#ff6a16'; // color all text
+var UseSmoke = true; //set smoke ON and OFF
+var TXT_Load = 'Loading...' //text on load box
+var TXT_Player = 'Pressione <b style="color:red">SPACE</b> para parar de tocar a música e <b style="color:red">+</b> ou <b style="color:red">-</b> para alterar o volume da música' //text on load box
